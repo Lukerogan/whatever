4c3d2ec4bbd2d4c7de669b07c24f5c0c0305e7bf
Luke Rogan <126Lrogan@frhsd.com>
2024-10-28 22:36:56 -0400

    Done?

--- js/whatever.js
+++ js/whatever.js
@@ -1,15 +1,24 @@
 const d = new Date();
-let hour = d.getHours();
-// let hour = 
+// let hour = d.getHours();
+let hour = 2
+let greetingText = document.getElementById("greeting-text");
 
-if(hour <= 12){
-    console.log('monin');
-}
-else if(hour <17 && hour > 12){
-    console.log('afternoon');
-}
-else if(hour > 17){
-    console.log('night');
-}
-else{console.log('invalid time')}
-
+if (hour < 12) {
+    // if hour is less than 12pm
+    greetingText.textContent = "Good Morning!";
+    document.querySelector(".night").classList.add("d-none");
+    document.querySelector(".afternoon").classList.add("d-none");
+  } else if (hour < 17) {
+    // if hour is less than 17:00 (but greater than 12pm)
+    greetingText.textContent = "Good Afternoon!";
+    document.getElementsByTagName("body")[0].classList.add("noon-bg");
+    document.querySelector(".morning").classList.add("d-none");
+    document.querySelector(".night").classList.add("d-none");
+  } // hour is greater than 17:00
+  else {
+    greetingText.textContent = "Good Night!";
+    greetingText.parentElement.classList.add("night-gradient");
+    document.querySelector(".morning").classList.add("d-none");
+    document.querySelector(".afternoon").classList.add("d-none");
+  }
+  
