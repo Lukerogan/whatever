9e11ce4ae5a5b2f38101553b0ad2d5abc37d42e8
Lukerogan <126Lrogan@frhsd.com>
2024-10-29 13:57:26 -0400

    donefornow

--- js/whatever.js
+++ js/whatever.js
@@ -6,18 +6,19 @@ let greetingText = document.getElementById("greeting-text");
 if (hour < 12) {
     // if hour is less than 12pm
     greetingText.textContent = "Good Morning!";
+    document.getElementsById("solarnlunarmass")[0].classList.add("morning-bg");
     document.querySelector(".night").classList.add("d-none");
     document.querySelector(".afternoon").classList.add("d-none");
   } else if (hour < 17) {
     // if hour is less than 17:00 (but greater than 12pm)
     greetingText.textContent = "Good Afternoon!";
-    document.getElementsByTagName("body")[0].classList.add("noon-bg");
+    document.getElementsById("solarnlunarmass")[0].classList.add("noon-bg");
     document.querySelector(".morning").classList.add("d-none");
     document.querySelector(".night").classList.add("d-none");
   } // hour is greater than 17:00
   else {
     greetingText.textContent = "Good Night!";
-    greetingText.parentElement.classList.add("night-gradient");
+    greetingText.getElementById("solarnlunarmass").classList.add("night-gradient");
     document.querySelector(".morning").classList.add("d-none");
     document.querySelector(".afternoon").classList.add("d-none");
   }
